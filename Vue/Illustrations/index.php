359375fcd74593673e14d0a62713460351da02c4
Werner Burat <werner.burat@gmail.com>
2019-02-04 18:59:12 -0500

    Test images

--- Vue/Illustrations/index.php
+++ Vue/Illustrations/index.php
@@ -7,11 +7,11 @@
     <div class="row">
         <div class="col-sm-4"><img src="src/img/gallery/1.jpg" class="gallery"/></div>
 
-        <div class="col-sm-8"><img src="src/img/gallery/2.jpg" class="gallery"/></div>
+        <div class="col-sm-8"><img src="src/img/gallery/6.jpg" class="gallery"/></div>
 
         <div class="w-100 separator"></div>
 
-        <div class="col"><img src="src/img/gallery/3.jpg" class="gallery"/></div>
-        <div class="col"><img src="src/img/gallery/4.jpg" class="gallery"/></div>
+        <div class="col"><img src="src/img/gallery/8.jpg" class="gallery"/></div>
+        <div class="col"><img src="src/img/gallery/10.jpg" class="gallery"/></div>
     </div>
 </div>
