cf17a80b2661146e7c95b0fce59ade2663630707
Werner Burat <werner.burat@gmail.com>
2019-02-04 18:55:00 -0500

    PHP framework

---
+++ Vue/Illustrations/index.php
@@ -0,0 +1,17 @@
+
+<div class="container">
+
+    <div class="w-100 separator"></div>
+    <div class="w-100 separator"></div>
+
+    <div class="row">
+        <div class="col-sm-4"><img src="src/img/gallery/1.jpg" class="gallery"/></div>
+
+        <div class="col-sm-8"><img src="src/img/gallery/2.jpg" class="gallery"/></div>
+
+        <div class="w-100 separator"></div>
+
+        <div class="col"><img src="src/img/gallery/3.jpg" class="gallery"/></div>
+        <div class="col"><img src="src/img/gallery/4.jpg" class="gallery"/></div>
+    </div>
+</div>
